f576b58917c18168b4f098d698a23d1662f749de
jasonandjay <342690199@qq.com>
2021-08-06 15:27:00 +0800

    change mock Data to fastmock

--- react-big-screen/src/services/index.js
+++ react-big-screen/src/services/index.js
@@ -2,21 +2,21 @@ import request from '../utils/request';
 
 // 获取左侧界面数据请求
 export const getLeftPageData = async () => {
-  return request('/api/leftPageData').then(response => {
+  return request('https://www.fastmock.site/mock/501cc679d21dd9b5b72e155c068e0b75/bigscreen/api/leftPageData').then(response => {
     return response.data;
   });
 };
 
 // 获取中间界面数据请求
 export const getCenterPageData = async () => {
-  return request('/api/centerPageData').then(response => {
+  return request('https://www.fastmock.site/mock/501cc679d21dd9b5b72e155c068e0b75/bigscreen/api/centerPageData').then(response => {
     return response.data;
   });
 };
 
 // 获取右侧界面数据请求
 export const getRightPageData = async () => {
-  return request('/api/rightPageData').then(response => {
+  return request('https://www.fastmock.site/mock/501cc679d21dd9b5b72e155c068e0b75/bigscreen/api/rightPageData').then(response => {
     return response.data;
   });
 };
